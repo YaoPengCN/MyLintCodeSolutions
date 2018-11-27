7623ca9e5b854ab4a6a8c3e0875d7967ac0d53dd
P Yao <yaopengcn@outlook.com>
2018-11-27 16:58:36 +0800

    challenge problem 1.

--- 1. A + B Problem/1. A + B Problem.cpp
+++ 1. A + B Problem/1. A + B Problem.cpp
@@ -24,17 +24,42 @@
 */
 
 /**
+ * Challenge: do not use + or any arithmetic operators.
  * Run Time: 6ms
  */
 class Solution {
 public:
-    /*
-     * @param a: An integer
-     * @param b: An integer
-     * @return: The sum of a and b 
-     */
-    int aplusb(int a, int b) {
-        return a+b;
-    }
+	/**
+	 * @param a: An integer
+	 * @param b: An integer
+	 * @return: The sum of a and b
+	 */
+	// using XOR ^, "a ^ b" means "a + b" bit by bit without carrying
+	// "a & b << 1" records the carrying on each bit. 
+	// Thus, a + b = (a ^ b) + (a & b << 1)
+	int aplusb(int a, int b) {
+		while (b != 0) {
+			int _a = a ^ b;
+			int _b = (a & b) << 1;
+			a = _a;
+			b = _b;
+		}
+		return a;
+	}
 };
+
+///**
+// * Run Time: 6ms
+// */
+///* class Solution {
+//public:
+//    /*
+//     * @param a: An integer
+//     * @param b: An integer
+//     * @return: The sum of a and b 
+//     */
+//    int aplusb(int a, int b) {
+//       return a+b;
+//    }
+//};
 
