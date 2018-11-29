085045ed1ee000bfdd7fe52c3e06807b6b16d24f
P Yao <yaopengcn@outlook.com>
2018-11-29 22:45:19 +0800

    challenge problem 6.

--- 6. Merge Two Sorted Arrays/6. Merge Two Sorted Arrays.cpp
+++ 6. Merge Two Sorted Arrays/6. Merge Two Sorted Arrays.cpp
@@ -34,24 +34,81 @@ public:
 	 * @param B: sorted integer array B
 	 * @return: A new sorted integer array
 	 */
+	// use binary search for the case when one array is very large and the other.
 	vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
 		//write your code here
-		vector<int> result;
-		vector<int>::size_type ix = 0, jx = 0;
-		while (ix != A.size() && jx != B.size()) {
-			if (A[ix] < B[jx]) {
-				result.push_back(A[ix++]);
-			}
-			else {
-				result.push_back(B[jx++]);
-			}
+		vector<int> result, source;
+		vector<int>::size_type ix = 0, position = 0;
+		vector<int>::iterator iter;
+
+		if (A.empty())
+			return B;
+		if (B.empty())
+			return A;
+
+		if (A.size() > B.size()){
+			result = A;
+			source = B;
 		}
-		if (ix != A.size()) {
-			result.insert(result.end(), A.begin() + ix, A.end());
+		else {
+			result = B;
+			source = A;
 		}
-		else if (jx != B.size()) {
-			result.insert(result.end(), B.begin() + jx, B.end());
+		while (ix != source.size()) {
+			position = binarySearch(result, position, source[ix]);
+			iter = result.begin();
+			result.insert(iter+position, source[ix++]);
 		}
 		return result;
 	}
+private:
+	vector<int>::size_type binarySearch(vector<int> &Temp, vector<int>::size_type left, int target){
+		vector<int>::size_type right;
+		right = Temp.size() - 1;
+		while (left <= right) {
+			vector<int>::size_type mid = left + (right - left) / 2;
+			if (Temp[mid] == target)
+				return mid;
+			if (target < Temp[mid])
+				if (mid != 0)
+					right = mid - 1;
+				else
+					return mid;
+			else
+				left = mid + 1;
+		}
+		return left;
+	}
 };
+
+///**
+// * Run Time: 6ms
+// */
+//class Solution {
+//public:
+//	/**
+//	 * @param A: sorted integer array A
+//	 * @param B: sorted integer array B
+//	 * @return: A new sorted integer array
+//	 */
+//	vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
+//		//write your code here
+//		vector<int> result;
+//		vector<int>::size_type ix = 0, jx = 0;
+//		while (ix != A.size() && jx != B.size()) {
+//			if (A[ix] < B[jx]) {
+//				result.push_back(A[ix++]);
+//			}
+//			else {
+//				result.push_back(B[jx++]);
+//			}
+//		}
+//		if (ix != A.size()) {
+//			result.insert(result.end(), A.begin() + ix, A.end());
+//		}
+//		else if (jx != B.size()) {
+//			result.insert(result.end(), B.begin() + jx, B.end());
+//		}
+//		return result;
+//	}
+//};
