41b1684a5e4fa5475e5b5a5ff8d12b322221571e
P Yao <yaopengcn@outlook.com>
2020-09-28 09:56:29 +0800

    add problem 37. Reverse 3-digit Integer

---
+++ 37. Reverse 3-digit Integer/37. Reverse 3-digit Integer.cpp
@@ -0,0 +1,47 @@
+/** 
+* 37. Reverse 3-digit Integer
+* Difficulty
+* Easy
+* 
+* Description
+* Reverse a 3-digit integer.
+* 
+* Example
+* Example 1
+* Input: number = 123
+* Output: 321
+* 
+* Example 2
+* Input: number = 900
+* Output: 9
+*
+* Related Problems 
+* 767. Reverse Array
+* 413. Reverse Integer
+*/
+
+/**
+ * Run Time:  50ms
+ */
+class Solution {
+public:
+    /**
+     * @param number: A 3-digit number.
+     * @return: Reversed number.
+     */
+    int reverseInteger(int number) {
+        vector<int> digit(3);
+        int reversed = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            digit[i] = number % 10;
+            number /= 10;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            reversed *= 10;
+            reversed += digit[i];            
+        }
+        return reversed;
+    }
+};
