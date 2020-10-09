cbb05c8aa0aa614a4d2e1587e7acb9cf1ee629d8
P Yao <yaopengcn@outlook.com>
2020-10-09 10:54:59 +0800

    challenge problem 66. Binary Tree Preorder Traversal with Stack

--- 66. Binary Tree Preorder Traversal/66. Binary Tree Preorder Traversal.cpp
+++ 66. Binary Tree Preorder Traversal/66. Binary Tree Preorder Traversal.cpp
@@ -46,10 +46,9 @@
  * Challenge
  * Can you do it without recursion?
  * 
- * Morris Algorithm
+ * Stack
  * Ref[1]: https://www.jiuzhang.com/problem/binary-tree-preorder-traversal/#tag-lang-ALL
- * Ref[2]: https://zhuanlan.zhihu.com/p/101321696
- * Run Time: 50ms
+ * Run Time: 101ms
  */
 /**
  * Definition of TreeNode:
@@ -64,40 +63,95 @@
  * }
  */
 
-class Solution {
+class Solution
+{
 public:
     /**
      * @param root: A Tree
      * @return: Preorder in ArrayList which contains node values.
      */
-    vector<int> preorderTraversal(TreeNode * root) {
-        vector<int> res;
-        TreeNode *cur = NULL;
-        
-        while (root != NULL) {
-            if (root->left != NULL) {
-                cur = root->left;
-                while (cur->right != NULL && cur->right != root) {
-                    cur = cur->right;
-                }
-                if (cur->right == root) {
-                    cur->right = NULL;
-                    root = root->right;
-                }
-                else {
-                    res.push_back(root->val);
-                    cur->right = root;
-                    root = root->left;
-                }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        if (!root)
+            return result;
+        stack<TreeNode *> st;
+        st.push(root);
+        while (!st.empty())
+        {
+            TreeNode *node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->right)
+            {
+                st.push(node->right);
             }
-            else {
-                res.push_back(root->val);
-                root = root->right;
+            if (node->left)
+            {
+                st.push(node->left);
             }
         }
-        return res;
+        return result;
     }
 };
+
+// /**
+//  * Challenge
+//  * Can you do it without recursion?
+//  *
+//  * Morris Algorithm
+//  * Ref[1]: https://www.jiuzhang.com/problem/binary-tree-preorder-traversal/#tag-lang-ALL
+//  * Ref[2]: https://zhuanlan.zhihu.com/p/101321696
+//  * Ref[3]: https://zhuanlan.zhihu.com/p/89858150
+//  * Run Time: 50ms
+//  */
+// /**
+//  * Definition of TreeNode:
+//  * class TreeNode {
+//  * public:
+//  *     int val;
+//  *     TreeNode *left, *right;
+//  *     TreeNode(int val) {
+//  *         this->val = val;
+//  *         this->left = this->right = NULL;
+//  *     }
+//  * }
+//  */
+
+// class Solution {
+// public:
+//     /**
+//      * @param root: A Tree
+//      * @return: Preorder in ArrayList which contains node values.
+//      */
+//     vector<int> preorderTraversal(TreeNode * root) {
+//         vector<int> res;
+//         TreeNode *cur = NULL;
+
+//         while (root != NULL) {
+//             if (root->left != NULL) {
+//                 cur = root->left;
+//                 while (cur->right != NULL && cur->right != root) {
+//                     cur = cur->right;
+//                 }
+//                 if (cur->right == root) {
+//                     cur->right = NULL;
+//                     root = root->right;
+//                 }
+//                 else {
+//                     res.push_back(root->val);
+//                     cur->right = root;
+//                     root = root->left;
+//                 }
+//             }
+//             else {
+//                 res.push_back(root->val);
+//                 root = root->right;
+//             }
+//         }
+//         return res;
+//     }
+// };
 
 // /**
 //  * Divide and conquer
