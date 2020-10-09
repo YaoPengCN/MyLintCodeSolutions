92daf62f07f76a19b12e672c167c009ad9fbe8fa
P Yao <yaopengcn@outlook.com>
2020-10-09 14:29:42 +0800

    challenge problem 68. Binary Tree Postorder Traversal with Stack

--- 68. Binary Tree Postorder Traversal/68. Binary Tree Postorder Traversal.cpp
+++ 68. Binary Tree Postorder Traversal/68. Binary Tree Postorder Traversal.cpp
@@ -45,10 +45,8 @@
  * Challenge
  * Can you do it without recursion?
  *
- * Morris Algorithm
+ * Stack
  * Ref[1]: https://www.jiuzhang.com/problem/binary-tree-postorder-traversal/#tag-lang-cpp
- * Ref[2]: https://zhuanlan.zhihu.com/p/101321696
- * Ref[3]: https://zhuanlan.zhihu.com/p/89858150
  * Run Time: 50ms
  */
 /**
@@ -64,49 +62,101 @@
  * }
  */
 
-class Solution
-{
+class Solution {
 public:
     /**
      * @param root: A Tree
      * @return: Postorder in ArrayList which contains node values.
      */
-    vector<int> postorderTraversal(TreeNode *root)
-    {
-        vector<int> res;
-        TreeNode *cur = NULL;
-
-        while (root != NULL)
+    vector<int> postorderTraversal(TreeNode * root) {
+        stack<TreeNode *> st;
+        vector<int> result;
+        if(root == NULL)
         {
-            if (root->right != NULL)
-            {
-                cur = root->right;
-                while (cur->left && cur->left != root)
-                {
-                    cur = cur->left;
-                }
-                if (cur->left == root)
-                {
-                    cur->left = NULL;
-                    root = root->left;
-                }
-                else
-                {
-                    res.push_back(root->val);
-                    cur->left = root;
-                    root = root->right;
-                }
-            }
-            else
-            {
-                res.push_back(root->val);
-                root = root->left;
-            }
+            return result;
         }
-        reverse(res.begin(), res.end());
-        return res;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode *temp = st.top();
+            st.pop();
+            result.insert(result.begin(), temp->val);
+            if(temp->left != NULL)
+                st.push(temp->left);
+            if(temp->right != NULL)
+                st.push(temp->right);
+        }
+        return result;
     }
 };
+
+
+// /**
+//  * Challenge
+//  * Can you do it without recursion?
+//  *
+//  * Morris Algorithm
+//  * Ref[1]: https://www.jiuzhang.com/problem/binary-tree-postorder-traversal/#tag-lang-cpp
+//  * Ref[2]: https://zhuanlan.zhihu.com/p/101321696
+//  * Ref[3]: https://zhuanlan.zhihu.com/p/89858150
+//  * Run Time: 50ms
+//  */
+// /**
+//  * Definition of TreeNode:
+//  * class TreeNode {
+//  * public:
+//  *     int val;
+//  *     TreeNode *left, *right;
+//  *     TreeNode(int val) {
+//  *         this->val = val;
+//  *         this->left = this->right = NULL;
+//  *     }
+//  * }
+//  */
+
+// class Solution
+// {
+// public:
+//     /**
+//      * @param root: A Tree
+//      * @return: Postorder in ArrayList which contains node values.
+//      */
+//     vector<int> postorderTraversal(TreeNode *root)
+//     {
+//         vector<int> res;
+//         TreeNode *cur = NULL;
+
+//         while (root != NULL)
+//         {
+//             if (root->right != NULL)
+//             {
+//                 cur = root->right;
+//                 while (cur->left && cur->left != root)
+//                 {
+//                     cur = cur->left;
+//                 }
+//                 if (cur->left == root)
+//                 {
+//                     cur->left = NULL;
+//                     root = root->left;
+//                 }
+//                 else
+//                 {
+//                     res.push_back(root->val);
+//                     cur->left = root;
+//                     root = root->right;
+//                 }
+//             }
+//             else
+//             {
+//                 res.push_back(root->val);
+//                 root = root->left;
+//             }
+//         }
+//         reverse(res.begin(), res.end());
+//         return res;
+//     }
+// };
 
 // /**
 //  * Divide and conquer
