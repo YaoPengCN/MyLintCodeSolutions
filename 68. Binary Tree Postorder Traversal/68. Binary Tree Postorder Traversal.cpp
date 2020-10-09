b17884a7ae470b51112d69d0f7a566f09d836218
P Yao <yaopengcn@outlook.com>
2020-10-09 14:20:09 +0800

    challenge problem 68. Binary Tree Postorder Traversal with Morris Algorithm

--- 68. Binary Tree Postorder Traversal/68. Binary Tree Postorder Traversal.cpp
+++ 68. Binary Tree Postorder Traversal/68. Binary Tree Postorder Traversal.cpp
@@ -42,7 +42,13 @@
 */
 
 /**
- * Divide and conquer
+ * Challenge
+ * Can you do it without recursion?
+ *
+ * Morris Algorithm
+ * Ref[1]: https://www.jiuzhang.com/problem/binary-tree-postorder-traversal/#tag-lang-cpp
+ * Ref[2]: https://zhuanlan.zhihu.com/p/101321696
+ * Ref[3]: https://zhuanlan.zhihu.com/p/89858150
  * Run Time: 50ms
  */
 /**
@@ -67,15 +73,76 @@ public:
      */
     vector<int> postorderTraversal(TreeNode *root)
     {
-        vector<int> result;
-        if (!root)
-            return result;
-        vector<int> leftResult = postorderTraversal(root->left);
-        vector<int> midResult = {root->val};
-        vector<int> rightResult = postorderTraversal(root->right);
-        result.insert(result.end(), leftResult.begin(), leftResult.end());
-        result.insert(result.end(), rightResult.begin(), rightResult.end());
-        result.insert(result.end(), midResult.begin(), midResult.end());
-        return result;
+        vector<int> res;
+        TreeNode *cur = NULL;
+
+        while (root != NULL)
+        {
+            if (root->right != NULL)
+            {
+                cur = root->right;
+                while (cur->left && cur->left != root)
+                {
+                    cur = cur->left;
+                }
+                if (cur->left == root)
+                {
+                    cur->left = NULL;
+                    root = root->left;
+                }
+                else
+                {
+                    res.push_back(root->val);
+                    cur->left = root;
+                    root = root->right;
+                }
+            }
+            else
+            {
+                res.push_back(root->val);
+                root = root->left;
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
+
+// /**
+//  * Divide and conquer
+//  * Run Time: 50ms
+//  */
+// /**
+//  * Definition of TreeNode:
+//  * class TreeNode {
+//  * public:
+//  *     int val;
+//  *     TreeNode *left, *right;
+//  *     TreeNode(int val) {
+//  *         this->val = val;
+//  *         this->left = this->right = NULL;
+//  *     }
+//  * }
+//  */
+
+// class Solution
+// {
+// public:
+//     /**
+//      * @param root: A Tree
+//      * @return: Postorder in ArrayList which contains node values.
+//      */
+//     vector<int> postorderTraversal(TreeNode *root)
+//     {
+//         vector<int> result;
+//         if (!root)
+//             return result;
+//         vector<int> leftResult = postorderTraversal(root->left);
+//         vector<int> midResult = {root->val};
+//         vector<int> rightResult = postorderTraversal(root->right);
+//         result.insert(result.end(), leftResult.begin(), leftResult.end());
+//         result.insert(result.end(), rightResult.begin(), rightResult.end());
+//         result.insert(result.end(), midResult.begin(), midResult.end());
+//         return result;
+//     }
+// };
