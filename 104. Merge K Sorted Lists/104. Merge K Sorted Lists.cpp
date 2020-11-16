377f819347ba2d1e54690c1e3a40272a92ac57fe
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2020-11-16 10:58:59 +0800

    improve problem 104. Merge K Sorted Lists with priority_queue

--- 104. Merge K Sorted Lists/104. Merge K Sorted Lists.cpp
+++ 104. Merge K Sorted Lists/104. Merge K Sorted Lists.cpp
@@ -25,9 +25,10 @@
  */
 
 /**
- * Two pointers
- * Ref: https://www.jiuzhang.com/problem/merge-k-sorted-lists/#tag-lang-cpp
- * Running Time: 686ms
+ * Priority_queue
+ * Ref[1]: https://www.jiuzhang.com/problem/merge-k-sorted-lists/#tag-lang-cpp
+ * Ref[2]: https://blog.51cto.com/9250070/2506737
+ * Running Time: 352ms
  */
 /**
  * Definition of ListNode
@@ -50,54 +51,98 @@ public:
      */
 	ListNode *mergeKLists(vector<ListNode *> &lists)
 	{
-		// write your code here
-		ListNode *tmp_node = NULL;
-		ListNode *tar_node = NULL;
-		ListNode *new_node = NULL;
-		ListNode *result = NULL;
-		for (int i = 0; i < lists.size(); ++i)
-		{
-			tmp_node = lists[i];
-			if (NULL == tmp_node)
-			{
-				continue;
-			}
-			if (NULL == result)
-			{
-				result = new ListNode(tmp_node->val);
-				tmp_node = tmp_node->next;
-			}
-			else
-			{
-				if (result->val > tmp_node->val)
-				{
-					new_node = new ListNode(tmp_node->val);
-					new_node->next = result;
-					result = new_node;
-					tmp_node = tmp_node->next;
-				}
-			}
-			tar_node = result;
-			while (tmp_node != NULL)
-			{
-				while (NULL != tar_node->next && tar_node->next->val < tmp_node->val)
-				{
-					tar_node = tar_node->next;
-				}
-				if (NULL == tar_node->next)
-				{
-					new_node = new ListNode(tmp_node->val);
-					tar_node->next = new_node;
-				}
-				else
-				{
-					new_node = new ListNode(tmp_node->val);
-					new_node->next = tar_node->next;
-					tar_node->next = new_node;
-				}
-				tmp_node = tmp_node->next;
-			}
-		}
-		return result;
+        auto comp=[](ListNode* l,ListNode* r){return l->val>r->val;};
+        priority_queue<ListNode*,vector<ListNode*>,decltype(comp)> Q(comp);
+        ListNode *head=nullptr,**cur=&head;
+        for(auto l:lists)
+            if(l)
+                Q.push(l);
+        while(!Q.empty()){
+            auto q=Q.top();
+            Q.pop();
+            *cur=q;
+            cur=&q->next;
+            if(*cur)
+                Q.push(*cur);
+        }
+        return head;
 	}
 };
+
+// /**
+//  * Two pointers
+//  * Ref: https://www.jiuzhang.com/problem/merge-k-sorted-lists/#tag-lang-cpp
+//  * Running Time: 686ms
+//  */
+// /**
+//  * Definition of ListNode
+//  * class ListNode {
+//  * public:
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int val) {
+//  *         this->val = val;
+//  *         this->next = NULL;
+//  *     }
+//  * }
+//  */
+// class Solution
+// {
+// public:
+// 	/**
+//      * @param lists: a list of ListNode
+//      * @return: The head of one sorted list.
+//      */
+// 	ListNode *mergeKLists(vector<ListNode *> &lists)
+// 	{
+// 		// write your code here
+// 		ListNode *tmp_node = NULL;
+// 		ListNode *tar_node = NULL;
+// 		ListNode *new_node = NULL;
+// 		ListNode *result = NULL;
+// 		for (int i = 0; i < lists.size(); ++i)
+// 		{
+// 			tmp_node = lists[i];
+// 			if (NULL == tmp_node)
+// 			{
+// 				continue;
+// 			}
+// 			if (NULL == result)
+// 			{
+// 				result = new ListNode(tmp_node->val);
+// 				tmp_node = tmp_node->next;
+// 			}
+// 			else
+// 			{
+// 				if (result->val > tmp_node->val)
+// 				{
+// 					new_node = new ListNode(tmp_node->val);
+// 					new_node->next = result;
+// 					result = new_node;
+// 					tmp_node = tmp_node->next;
+// 				}
+// 			}
+// 			tar_node = result;
+// 			while (tmp_node != NULL)
+// 			{
+// 				while (NULL != tar_node->next && tar_node->next->val < tmp_node->val)
+// 				{
+// 					tar_node = tar_node->next;
+// 				}
+// 				if (NULL == tar_node->next)
+// 				{
+// 					new_node = new ListNode(tmp_node->val);
+// 					tar_node->next = new_node;
+// 				}
+// 				else
+// 				{
+// 					new_node = new ListNode(tmp_node->val);
+// 					new_node->next = tar_node->next;
+// 					tar_node->next = new_node;
+// 				}
+// 				tmp_node = tmp_node->next;
+// 			}
+// 		}
+// 		return result;
+// 	}
+// };
