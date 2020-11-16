829d8ff65f2fe03a685110ebbff366d76697f5c2
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2020-11-16 10:48:59 +0800

    add problem 104. Merge K Sorted Lists with two pointers

---
+++ 104. Merge K Sorted Lists/104. Merge K Sorted Lists.cpp
@@ -0,0 +1,103 @@
+/** 
+ * 104. Merge K Sorted Lists
+ * Difficulty
+ * Medium
+ * 
+ * Description
+ * Merge k sorted linked lists and return it as one sorted list.
+ * Analyze and describe its complexity.
+ *
+ * Example
+ * Example 1
+ * Input: [2->4->null,null,-1->null]
+ * Output: -1->2->4->null
+ * 
+ * Example 2
+ * Input: [2->6->null,5->null,7->null]
+ * Output: 2->5->6->7->null
+ * 
+ * Related Problems 
+ * 839. Merge Two Sorted Interval Lists
+ * 791. Merge Number
+ * 486. Merge K Sorted Arrays
+ * 6. Merge Two Sorted Arrays
+ * 4. Ugly Number II
+ */
+
+/**
+ * Two pointers
+ * Ref: https://www.jiuzhang.com/problem/merge-k-sorted-lists/#tag-lang-cpp
+ * Running Time: 686ms
+ */
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution
+{
+public:
+	/**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+	ListNode *mergeKLists(vector<ListNode *> &lists)
+	{
+		// write your code here
+		ListNode *tmp_node = NULL;
+		ListNode *tar_node = NULL;
+		ListNode *new_node = NULL;
+		ListNode *result = NULL;
+		for (int i = 0; i < lists.size(); ++i)
+		{
+			tmp_node = lists[i];
+			if (NULL == tmp_node)
+			{
+				continue;
+			}
+			if (NULL == result)
+			{
+				result = new ListNode(tmp_node->val);
+				tmp_node = tmp_node->next;
+			}
+			else
+			{
+				if (result->val > tmp_node->val)
+				{
+					new_node = new ListNode(tmp_node->val);
+					new_node->next = result;
+					result = new_node;
+					tmp_node = tmp_node->next;
+				}
+			}
+			tar_node = result;
+			while (tmp_node != NULL)
+			{
+				while (NULL != tar_node->next && tar_node->next->val < tmp_node->val)
+				{
+					tar_node = tar_node->next;
+				}
+				if (NULL == tar_node->next)
+				{
+					new_node = new ListNode(tmp_node->val);
+					tar_node->next = new_node;
+				}
+				else
+				{
+					new_node = new ListNode(tmp_node->val);
+					new_node->next = tar_node->next;
+					tar_node->next = new_node;
+				}
+				tmp_node = tmp_node->next;
+			}
+		}
+		return result;
+	}
+};
