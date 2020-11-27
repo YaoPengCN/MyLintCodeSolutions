03e32bd1afdb27f4a6d49fc6dd223150dc4aa66a
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2020-11-27 17:58:30 +0800

    stash problem 392. House Robber

---
+++ 392. House Robber/392. House Robber.cpp
@@ -0,0 +1,57 @@
+/** 
+ * 392. House Robber
+ * 
+ * Difficulty
+ * Medium
+ * 
+ * Description
+ * You are a professional robber planning to rob houses along a street. 
+ * Each house has a certain amount of money stashed, 
+ * the only constraint stopping you from robbing each of them is that adjacent houses have security system connected,
+ * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ * 
+ * Example
+ * Example 1
+ * Input: [3, 8, 4]
+ * Output: 8
+ * Explanation: Just rob the second house.
+ * 
+ * Example 2:
+ * Input: [5, 2, 1, 3]
+ * Output: 8
+ * Explanation: Rob the first and the last house.
+ * 
+ * Challenge
+ * O(n) time and O(1) memory.
+ *  
+ * Related Problems 
+ * 1142. Non-negative Integers without Consecutive Ones
+ * 866. Coin Path
+ * 535. House Robber III
+ * 534. House Robber II
+ * 515. Paint House
+ * 514. Paint Fence
+ * 366. Fibonacci
+ * 111. Climbing Stairs
+ */
+
+/**
+ * Challenge
+ * O(n) time and O(1) memory.
+ * 
+ * Dynamic programming
+ * Ref[1]: https://mp.weixin.qq.com/s/z44hk0MW14_mAQd7988mfw
+ * Ref[2]: https://www.jiuzhang.com/problem/house-robber/#tag-lang-cpp
+ * Running Time: ms
+ */
+class Solution {
+public:
+    /**
+     * @param A: An array of non-negative integers
+     * @return: The maximum amount of money you can rob tonight
+     */
+    long long houseRobber(vector<int> &A) {
+        // write your code here
+    }
+};
