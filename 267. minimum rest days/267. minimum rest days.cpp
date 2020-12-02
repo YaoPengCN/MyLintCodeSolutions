31e18811e6684408d96b30a85016f1078d6191dc
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2020-12-02 15:52:19 +0800

    improbe problem 267. minimum rest days

--- 267. minimum rest days/267. minimum rest days.cpp
+++ 267. minimum rest days/267. minimum rest days.cpp
@@ -36,11 +36,11 @@
  * 50. Product of Array Exclude Itself
  */
 
-/** 
+/**
  * Dynamic programming
  * Ref[1]: https://www.jiuzhang.com/problem/minimum-rest-days/
- * 
- * Running Time: 100ms
+ *
+ * Running Time: 50ms
  */
 class Solution
 {
@@ -55,40 +55,104 @@ public:
         if (company.empty() || gym.empty() || (company.size() != gym.size()))
             return -1;
 
-        // dp(n, 3)
+        // dp size: (n, 3)
         // dp[i][0] for the minimum rest days if work in the i-th day
         // dp[i][1] for the minimum rest days if exercise in the i-th day
         // dp[i][2] for the minimum rest days if rest in the i-th day
-        vector<vector<int>> dp(company.size());
-        for (vector<vector<int>>::size_type i = 0; i != company.size(); i++)
+        vector<vector<int>> dp(2);
+        for (vector<vector<int>>::size_type i = 0; i != 2; i++)
             for (vector<int>::size_type j = 0; j != 3; j++)
                 dp[i].push_back(INT_MAX);
 
         if (company[0] == 1)
         {
             dp[0][0] = 0;
+            dp[1][0] = 0;
         }
         if (gym[0] == 1)
         {
             dp[0][1] = 0;
+            dp[1][1] = 0;
         }
 
         dp[0][2] = 1;
+        dp[1][2] = 1;
 
         for (vector<vector<int>>::size_type i = 1; i != company.size(); i++)
         {
+            for (vector<int>::size_type j = 0; j != 3; j++)
+                dp[1][j] = INT_MAX;
+
             if (company[i] == 1)
             {
-                dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]);
+                dp[1][0] = min(dp[0][1], dp[0][2]);
             }
             if (gym[i] == 1)
             {
-                dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]);
+                dp[1][1] = min(dp[0][0], dp[0][2]);
             }
-            dp[i][2] = min(min(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]) + 1;
+            dp[1][2] = min(min(dp[0][0], dp[0][1]), dp[0][2]) + 1;
+
+            for (vector<int>::size_type j = 0; j != 3; j++)
+                dp[0][j] = dp[1][j];
         }
 
-        return min(min(dp[company.size() - 1][0], dp[company.size() - 1][1]), dp[company.size() - 1][2]);
+        return min(min(dp[1][0], dp[1][1]), dp[1][2]);
     }
 };
 
+// /**
+//  * Dynamic programming
+//  * Ref[1]: https://www.jiuzhang.com/problem/minimum-rest-days/
+//  *
+//  * Running Time: 100ms
+//  */
+// class Solution
+// {
+// public:
+//     /**
+//      * @param company: Company business
+//      * @param gym: Gym business
+//      * @return: Find the shortest rest day
+//      */
+//     int minimumRestDays(vector<int> &company, vector<int> &gym)
+//     {
+//         if (company.empty() || gym.empty() || (company.size() != gym.size()))
+//             return -1;
+
+//         // dp size: (n, 3)
+//         // dp[i][0] for the minimum rest days if work in the i-th day
+//         // dp[i][1] for the minimum rest days if exercise in the i-th day
+//         // dp[i][2] for the minimum rest days if rest in the i-th day
+//         vector<vector<int>> dp(company.size());
+//         for (vector<vector<int>>::size_type i = 0; i != company.size(); i++)
+//             for (vector<int>::size_type j = 0; j != 3; j++)
+//                 dp[i].push_back(INT_MAX);
+
+//         if (company[0] == 1)
+//         {
+//             dp[0][0] = 0;
+//         }
+//         if (gym[0] == 1)
+//         {
+//             dp[0][1] = 0;
+//         }
+
+//         dp[0][2] = 1;
+
+//         for (vector<vector<int>>::size_type i = 1; i != company.size(); i++)
+//         {
+//             if (company[i] == 1)
+//             {
+//                 dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]);
+//             }
+//             if (gym[i] == 1)
+//             {
+//                 dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]);
+//             }
+//             dp[i][2] = min(min(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]) + 1;
+//         }
+
+//         return min(min(dp[company.size() - 1][0], dp[company.size() - 1][1]), dp[company.size() - 1][2]);
+//     }
+// };
