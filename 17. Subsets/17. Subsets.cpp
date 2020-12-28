2976c1148d8e0f9a5a9237cf3d4adba277b36b99
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2020-12-28 09:53:29 +0800

    challenge problem 17. Subsets with BFS iteratively

--- 17. Subsets/17. Subsets.cpp
+++ 17. Subsets/17. Subsets.cpp
@@ -47,8 +47,11 @@
  * 18. Subsets II
  * */
 
-/**  
- * DFS + backtracking
+/**
+ * Challenge
+ * Can you do it in both recursively and iteratively?
+ *   
+ * BFS iteratively
  * Ref: https://www.jiuzhang.com/problem/subsets/#tag-lang-cpp
  * Running Time: 50ms
  */
@@ -63,31 +66,71 @@ public:
     vector<vector<int>> subsets(vector<int> &nums)
     {
         vector<vector<int>> res;
+        res.push_back({});
+        int idx = 0;
 
-        // nums must be sorted, or the solution would not pass
-        sort(nums.begin(), nums.end());
+        while (idx < res.size())
+        {
+            vector<int> subset = res[idx];
+            idx++;
+            for (int num : nums)
+            {
+                // the following condition is to sort the result
+                if (!subset.empty() && num <= subset.back())
+                {
+                    continue;
+                }
 
-        // dfs
-        vector<int> subset;
-        dfs(nums, 0, subset, res);
+                subset.push_back(num);
+                res.push_back(subset);
+                subset.pop_back();
+            }
+        }
         return res;
     }
-
-private:
-    void dfs(vector<int> &nums, int k, vector<int> &subset, vector<vector<int>> &res)
-    {
-        // store the subset into res
-        res.push_back(vector<int>(subset));
-        // add an element in the subset
-        for (int i = k; i < nums.size(); ++i)
-        {
-            subset.push_back(nums[i]);
-
-            // deeper dfs
-            dfs(nums, i + 1, subset, res);
-
-            // backtracking
-            subset.pop_back();
-        }
-    }
 };
+
+// /**
+//  * DFS + backtracking
+//  * Ref: https://www.jiuzhang.com/problem/subsets/#tag-lang-cpp
+//  * Running Time: 50ms
+//  */
+
+// class Solution
+// {
+// public:
+//     /**
+//      * @param nums: A set of numbers
+//      * @return: A list of lists
+//      */
+//     vector<vector<int>> subsets(vector<int> &nums)
+//     {
+//         vector<vector<int>> res;
+
+//         // nums must be sorted, or the solution would not pass
+//         sort(nums.begin(), nums.end());
+
+//         // dfs
+//         vector<int> subset;
+//         dfs(nums, 0, subset, res);
+//         return res;
+//     }
+
+// private:
+//     void dfs(vector<int> &nums, int k, vector<int> &subset, vector<vector<int>> &res)
+//     {
+//         // store the subset into res
+//         res.push_back(vector<int>(subset));
+//         // add an element in the subset
+//         for (int i = k; i < nums.size(); ++i)
+//         {
+//             subset.push_back(nums[i]);
+
+//             // deeper dfs
+//             dfs(nums, i + 1, subset, res);
+
+//             // backtracking
+//             subset.pop_back();
+//         }
+//     }
+// };
