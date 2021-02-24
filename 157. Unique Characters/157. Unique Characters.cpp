ba6be220ea4eff6eefb16bfc6408f3421681d546
P Yao <12227516+YaoPengCN@users.noreply.github.com>
2021-02-24 09:54:54 +0800

    add and challenge problem 157. Unique Characters

---
+++ 157. Unique Characters/157. Unique Characters.cpp
@@ -0,0 +1,80 @@
+/** 
+ * 157. Unique Characters
+ * 
+ * Difficulty
+ * Easy
+ * 
+ * Description
+ * Implement an algorithm to determine if a string has all unique characters.
+ * 
+ * Example
+ * Example 1
+ * Input: "abc_____"
+ * Output: false
+ * 
+ * Example 2:
+ * Input:  "abc"
+ * Output: true		
+ * 
+ * Challenge
+ * What if you can not use additional data structures?
+ * 
+ * Related Problems
+ * 209. First Unique Character in a String
+ */
+
+/**
+ * Challenge
+ * What if you can not use additional data structures?
+ * 
+ * Running Time: 407ms
+ */
+
+class Solution
+{
+public:
+    /*
+     * @param str: A string
+     * @return: a boolean
+     */
+    bool isUnique(string &str)
+    {
+        sort(str.begin(), str.end());
+
+        for (int i = 1; i != str.size(); i++)
+        {
+            if (str[i] == str[i - 1])
+                return false;
+        }
+
+        return true;
+    }
+};
+
+// /**
+//  * Challenge
+//  * What if you can not use additional data structures?
+//  * 
+//  * Running Time: 407ms
+//  */
+
+// class Solution
+// {
+// public:
+//     /*
+//      * @param str: A string
+//      * @return: a boolean
+//      */
+//     bool isUnique(string &str)
+//     {
+//         sort(str.begin(), str.end());
+
+//         for (int i = 1; i != str.size(); i++)
+//         {
+//             if (str[i] == str[i - 1])
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// };
